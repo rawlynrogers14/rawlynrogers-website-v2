effbc2eba56ee180b33c828ac2ae9963c0da295f
rawlynrogers14 <156336505+rawlynrogers14@users.noreply.github.com>
2026-05-20 13:12:28 -0400

    Initial baseline architecture setup

---
+++ src/main/java/com/rawlynrogers/rawlynrogerswebsite/RawlynRogersWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.rawlynrogers.rawlynrogerswebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RawlynRogersWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RawlynRogersWebsiteApplication.class, args);
+    }
+
+}
